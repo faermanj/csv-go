a78090aae443e5e8c6d3142482ed7260c410efe2
Julio Faerman <jfaerman@gmail.com>
2021-04-16 14:22:56 +0200

    HTML sucks

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       string(index),
+		Body:       string("Só bora \o/"),
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
