cc893e85bc615d95372b3b921c8cd00255b670e3
Julio Faerman <jfaerman@gmail.com>
2021-04-16 14:29:41 +0200

    No unused

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ import (
 // Handler is executed by AWS Lambda in the main function. Once the request
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
+	/*
 	index, err := ioutil.ReadFile("public/index.html")
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
 	}
-
+	*/
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       string("Só bora \\o/"),
